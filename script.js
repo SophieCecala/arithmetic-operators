1cda1ced666305ebfdee02517d48893008599376
Natalya Myunster <natalyamyunster@yahoo.com>
2020-12-03 15:27:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// var firstnumber = 5;
+// var secondnumber = 3;
+// var third number = firstnumber + secondnumber;
+// secondnumber = secondnumber - firstnumber;
+// thirdnumber = firstnumber - secondnumber;
+
+var firstNumber = 5;
+var secondNumber = 3;
+var a = firstNumber * secondNumber;
+var b = firstNumber / secondNumber;
+var c = 0 - firstNumber;
+var d = firstNumber % secondNumber;
+var e = firstNumber ** secondNumber;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
